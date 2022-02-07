69d9cd8490c259442a09eb621598e9938b559e6d
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2022-02-07 14:43:51 +0100

    Add SelfieMut and more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,52 @@
-use std::marker::PhantomData;
-use std::ops::Deref;
-use std::pin::Pin;
+#![no_std]
 
-pub struct RefHandle<T: ?Sized>(PhantomData<Box<T>>);
+use core::marker::PhantomData;
+use core::ops::{Deref, DerefMut};
+use core::pin::Pin;
 
-// TODO: 'static
-impl<'a, T: 'a + ?Sized> PinHeaded<'a> for RefHandle<T> {
-    type Ref = &'a T;
+unsafe fn transmute_pin<T: ?Sized>(pin: Pin<&T>) -> Pin<&'static T> {
+    ::core::mem::transmute(pin)
 }
 
-pub trait PinHeaded<'a> {
+unsafe fn transmute_pin_mut<T: ?Sized>(pin: Pin<&mut T>) -> Pin<&'static mut T> {
+    ::core::mem::transmute(pin)
+}
+
+pub trait RefType<'a> {
     type Ref: 'a + Sized;
 }
 
-pub struct PinHead<P: Deref, H: for<'a> PinHeaded<'a> + ?Sized> {
-    pinned: Pin<P>,
-    referential: <H as PinHeaded<'static>>::Ref,
+pub struct Ref<T: ?Sized>(PhantomData<T>);
+// TODO: 'static
+impl<'a, T: 'a + ?Sized> RefType<'a> for Ref<T> {
+    type Ref = &'a T;
 }
 
-impl<P: Deref, H: for<'a> PinHeaded<'a> + ?Sized> PinHead<P, H> {
+pub struct Mut<T: ?Sized>(PhantomData<T>);
+
+// TODO: 'static
+impl<'a, T: 'a + ?Sized> RefType<'a> for Mut<T> {
+    type Ref = &'a mut T;
+}
+
+pub struct Selfie<P, R: for<'a> RefType<'a> + ?Sized> {
+    // SAFETY: enforce drop order!
+    referential: <R as RefType<'static>>::Ref,
+    pinned: Pin<P>,
+}
+
+impl<P: Deref, R: for<'a> RefType<'a> + ?Sized> Selfie<P, R> {
     pub fn new(
         pinned: Pin<P>,
-        handler: for<'a> fn(&'a P::Target) -> <H as PinHeaded<'a>>::Ref,
+        handler: for<'a> fn(&'a P::Target) -> <R as RefType<'a>>::Ref,
     ) -> Self
     where
         P::Target: 'static, // TODO
     {
-        let derefd = pinned.as_ref().get_ref();
-
         // SAFETY: derefd is pinned and cannot move, and this struct guarantees its lifetime
-        let derefd_static = unsafe { &*(derefd as *const _) };
-        let referential = handler(derefd_static);
+        let derefd = unsafe { transmute_pin(pinned.as_ref()) }.get_ref();
+
+        let referential = handler(derefd);
 
         Self {
             pinned,
@@ -43,8 +59,55 @@ impl<P: Deref, H: for<'a> PinHeaded<'a> + ?Sized> PinHead<P, H> {
         self.pinned.as_ref().get_ref()
     }
 
-    pub fn referential(&self) -> &<H as PinHeaded>::Ref {
+    #[inline]
+    pub fn referential(&self) -> &<R as RefType>::Ref {
         // TODO: check this is actually safe
         unsafe { ::core::mem::transmute(&self.referential) }
+    }
+
+    #[inline]
+    pub fn into_inner(self) -> Pin<P> {
+        self.pinned
+    }
+}
+
+pub struct SelfieMut<P, R: for<'a> RefType<'a> + ?Sized> {
+    // SAFETY: enforce drop order!
+    referential: <R as RefType<'static>>::Ref,
+    pinned: Pin<P>,
+}
+
+impl<P: DerefMut, R: for<'a> RefType<'a> + ?Sized> SelfieMut<P, R> {
+    pub fn new(
+        mut pinned: Pin<P>,
+        handler: for<'a> fn(Pin<&'a mut P::Target>) -> <R as RefType<'a>>::Ref,
+    ) -> Self
+    where
+        P::Target: 'static, // TODO
+    {
+        // SAFETY: derefd is pinned and cannot move, and this struct guarantees its lifetime
+        let derefd = unsafe { transmute_pin_mut(pinned.as_mut()) };
+
+        let referential = handler(derefd);
+
+        Self {
+            pinned,
+            referential,
+        }
+    }
+
+    pub fn referential(&self) -> &<R as RefType>::Ref {
+        // TODO: check this is actually safe
+        unsafe { ::core::mem::transmute(&self.referential) }
+    }
+
+    pub fn referential_mut(&mut self) -> &mut <R as RefType>::Ref {
+        // TODO: check this is actually safe
+        unsafe { ::core::mem::transmute(&mut self.referential) }
+    }
+
+    #[inline]
+    pub fn into_inner(self) -> Pin<P> {
+        self.pinned
     }
 }
--- tests/simple_ref.rs
+++ tests/simple_ref.rs
@@ -1,36 +1,103 @@
-use pinhead::{PinHead, PinHeaded, RefHandle};
+use selfie::{Ref, RefType, Selfie, SelfieMut};
 use std::pin::Pin;
 
-struct BoxAndRef {
-    data: PinHead<Box<i32>, RefHandle<i32>>,
-}
-
 #[test]
-pub fn simple_ref() {
+pub fn simple_int() {
     let my_int = Box::pin(42);
-    let data = PinHead::new(my_int, |i| i);
+    let data: Selfie<Box<i32>, Ref<i32>> = Selfie::new(my_int, |i| i);
 
-    let box_and_ref = BoxAndRef { data };
-    println!(
-        "{} <- {}",
-        box_and_ref.data.owned(),
-        box_and_ref.data.referential()
-    );
-}
+    assert_eq!(42, *data.owned());
+    assert_eq!(&42, *data.referential());
 
-struct BoxAndStr {
-    data: PinHead<Box<str>, RefHandle<str>>,
+    let data = Box::new(data);
+
+    assert_eq!(42, *data.owned());
+    assert_eq!(&42, *data.referential());
 }
 
 #[test]
 pub fn simple_str() {
-    let my_int = Pin::new("Hello, world!".to_owned().into_boxed_str());
-    let data = PinHead::new(my_int, |i| &i[0..5]);
+    let my_str = Pin::new("Hello, world!".to_owned().into_boxed_str());
+    let data: Selfie<Box<str>, Ref<str>> = Selfie::new(my_str, |i| &i[0..5]);
 
-    let box_and_ref = BoxAndStr { data };
-    println!(
-        "{} <- {}",
-        box_and_ref.data.owned(),
-        box_and_ref.data.referential()
-    );
+    assert_eq!("Hello, world!", data.owned());
+    assert_eq!(&"Hello", data.referential());
+
+    let val = data;
+    let data = Box::new(val);
+
+    assert_eq!("Hello, world!", data.owned());
+    assert_eq!(&"Hello", data.referential());
+}
+
+struct Point<'a> {
+    x: &'a mut i32,
+    y: &'a mut i32,
+}
+
+impl<'a> Point<'a> {
+    fn new(values: Pin<&'a mut (i32, i32)>) -> Self {
+        let values = Pin::into_inner(values);
+        Self {
+            x: &mut values.0,
+            y: &mut values.1,
+        }
+    }
+}
+
+struct PointMut;
+
+impl<'a> RefType<'a> for PointMut {
+    type Ref = Point<'a>;
+}
+
+#[test]
+pub fn struct_mut() {
+    let my_str = Box::pin((0, 42));
+    let mut data: SelfieMut<Box<(i32, i32)>, PointMut> = SelfieMut::new(my_str, |i| Point::new(i));
+
+    assert_eq!(0, *data.referential().x);
+    assert_eq!(42, *data.referential().y);
+    *data.referential_mut().x = 69;
+    assert_eq!(69, *data.referential().x);
+    assert_eq!(42, *data.referential().y);
+
+    let mut data = Box::new(data);
+
+    assert_eq!(69, *data.referential().x);
+    assert_eq!(42, *data.referential().y);
+    *data.referential_mut().x = 12;
+    assert_eq!(12, *data.referential().x);
+    assert_eq!(42, *data.referential().y);
+}
+
+struct Dropper<'a> {
+    value: &'a str,
+}
+
+impl<'a> Drop for Dropper<'a> {
+    fn drop(&mut self) {
+        assert_eq!("Hello", self.value)
+    }
+}
+
+struct DropperRef;
+
+impl<'a> RefType<'a> for DropperRef {
+    type Ref = Dropper<'a>;
+}
+
+#[test]
+pub fn drops() {
+    let my_str = Pin::new("Hello".to_owned().into_boxed_str());
+    let data: Selfie<Box<str>, DropperRef> = Selfie::new(my_str, |value| Dropper { value });
+
+    assert_eq!("Hello", data.owned());
+    assert_eq!("Hello", data.referential().value);
+
+    let data = Box::new(data);
+    assert_eq!("Hello", data.owned());
+    assert_eq!("Hello", data.referential().value);
+
+    drop(data);
 }
