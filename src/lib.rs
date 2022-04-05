5d99816cb2e8033595f8008080378b09c34a360f
Adrien Prokopowicz <adrien.prokopowicz@gmail.com>
2022-04-05 08:08:19 +0200

    More work figuring out ICE

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,60 @@
-#![no_std]
-#![deny(unsafe_code)] // Unsafe code will be allowed in specific modules on a case-by-case basis
-#![doc = include_str!("../README.md")]
+use core::pin::Pin;
+use std::marker::PhantomData;
+use std::ops::Deref;
 
-pub mod refs;
-pub(crate) mod utils;
+pub trait RefType<'a> {
+    type Ref: 'a + Sized;
+}
 
-mod selfie;
-pub use crate::selfie::*;
+pub struct Ref<T: ?Sized>(PhantomData<T>);
 
-mod safe;
+impl<'a, T: 'a + ?Sized> RefType<'a> for Ref<T> {
+    type Ref = &'a T;
+}
 
-pub mod convert;
+pub struct Selfie<P, R> {
+    i: PhantomData<(P, R)>,
+}
+
+impl<P, R> Selfie<P, R>
+where
+    P: Deref,
+    R: for<'this> RefType<'this>,
+{
+    #[inline]
+    pub fn new(
+        owned: Pin<P>,
+        handler: for<'this> fn(&'this P::Target) -> <R as RefType<'this>>::Ref,
+    ) {
+        Self::new_with(owned, handler)
+    }
+
+    pub fn new_with<F>(owned: Pin<P>, handler: F)
+    where
+        F: IntoReferential<P, R>,
+    {
+        let detached = owned.as_ref().get_ref();
+        let _ = handler.into_referential(detached);
+    }
+}
+
+pub trait IntoReferential<P, R>
+where
+    P: Deref,
+    R: for<'this> RefType<'this>,
+{
+    fn into_referential(self, owned: &P::Target) -> <R as RefType>::Ref;
+}
+
+impl<P: Deref, R: for<'this> RefType<'this>> IntoReferential<P, R>
+    for fn(&P::Target) -> <R as RefType>::Ref
+{
+    #[inline]
+    fn into_referential(self, owned: &P::Target) -> <R as RefType>::Ref {
+        self(owned)
+    }
+}
+
+pub fn example() {
+    let _ = Selfie::<Box<i32>, Ref<i32>>::new(Box::pin(42), |i| i);
+}
